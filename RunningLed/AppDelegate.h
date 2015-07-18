06c97585a88c4d491f392c20f320e8960df578bb
Hoang Tuan Anh <tuananhit91@gmail.com>
2015-07-18 22:06:26 +0700

    Initial Commit

---
+++ RunningLed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunningLed
+//
+//  Created by admin on 7/18/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RunningLed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RunningLed
+//
+//  Created by admin on 7/18/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
